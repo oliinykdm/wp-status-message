3c5434ea187a1241085ff3d1db63fc8dd0cf3770
oliynykdm <dima@mail.ua>
2022-01-26 11:46:40 +0200

    Create wp-status-message.php
    Initial file

---
+++ wp-status-message.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: WP Status Message
+Plugin URI: https://github.com/oliynykdm/wp-status-message
+Description: A great plugin to show the status in the WordPress Admin Dashboard (especially for WPMU DEV team)
+Author: Dima Oliynyk
+Version: 1.0
+Author URI: https://www.oliynyk.net
+*/
